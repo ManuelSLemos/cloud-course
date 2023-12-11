d50cf6c3ad4b5ae65a8af84dbd0ce837d8d52740
Manuel S. Lemos <manuelslemos@gmail.com>
2023-12-11 19:52:58 +0000

    sdfsdfsdfdsf

--- server.py
+++ server.py
@@ -7,9 +7,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Spain"}
 
 
-@app.get("/items/{item_id}")
+@app.get("pip install fastapi/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
